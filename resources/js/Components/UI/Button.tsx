b33f707cad0681a524b771f7e0213dff22ae8f00
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2025-02-21 17:53:34 +0000

    Initial upgrade

--- resources/js/Components/UI/Button.tsx
+++ resources/js/Components/UI/Button.tsx
@@ -6,7 +6,7 @@ import { LoadingSpinner } from '@/Components/LoadingSpinner'
 import { cn } from '@/Utils/utils'
 
 const buttonVariants = cva(
-  'inline-flex relative items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex relative items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
