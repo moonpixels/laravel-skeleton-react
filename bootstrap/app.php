0694762003e6c54a772a67a2ae601e8bde76c192
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2025-06-15 20:48:13 +0100

    Configure trusted proxies for Cloudflare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,7 +24,28 @@ return Application::configure(basePath: dirname(__DIR__))
                 SetLocale::class,
                 HandleInertiaRequests::class,
                 AddLinkHeadersForPreloadedAssets::class,
-            ]);
+            ])
+            // Configure trusted proxies for Cloudflare
+            ->trustProxies(
+                at: [
+                    '173.245.48.0/20',
+                    '103.21.244.0/22',
+                    '103.22.200.0/22',
+                    '103.31.4.0/22',
+                    '141.101.64.0/18',
+                    '108.162.192.0/18',
+                    '190.93.240.0/20',
+                    '188.114.96.0/20',
+                    '197.234.240.0/22',
+                    '198.41.128.0/17',
+                    '162.158.0.0/15',
+                    '104.16.0.0/13',
+                    '104.24.0.0/14',
+                    '172.64.0.0/13',
+                    '131.0.72.0/22',
+                ],
+                headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO,
+            );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->dontTruncateRequestExceptions();
