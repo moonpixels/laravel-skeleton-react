eaf43b0376437f3246b73fee472c30ec5ad25d29
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2025-06-16 11:26:07 +0100

    Add theme selection support with system preference for dark mode

--- resources/js/layouts/default-layout.tsx
+++ resources/js/layouts/default-layout.tsx
@@ -1,15 +1,15 @@
 import { Toaster } from '@/components/ui/sonner'
-import { DarkModeProvider } from '@/contexts/dark-mode-context'
+import { ThemeProvider } from '@/contexts/theme-context'
 import { UserProvider } from '@/contexts/user-context'
 import { PropsWithChildren } from 'react'
 
 export function DefaultLayout({ children }: PropsWithChildren) {
   return (
-    <DarkModeProvider>
+    <ThemeProvider>
       <UserProvider>
         {children}
         <Toaster />
       </UserProvider>
-    </DarkModeProvider>
+    </ThemeProvider>
   )
 }
