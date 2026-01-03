0797a59227afaf1f3d50ac18faae679b2cd6973e
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2026-01-03 16:38:21 +0000

    Refactor ESLint configuration for improved TypeScript support and plugin integration

--- eslint.config.js
+++ eslint.config.js
@@ -3,32 +3,34 @@ import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import reactPlugin from 'eslint-plugin-react'
 import reactHookPlugin from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
-export default [
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylisticTypeChecked,
   reactPlugin.configs.flat.recommended,
-  reactRefresh.configs.recommended,
+  reactHookPlugin.configs.flat.recommended,
+  reactRefresh.configs.vite,
   prettierRecommended,
   {
-    plugins: {
-      'react-hooks': reactHookPlugin,
-    },
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: './tsconfig.json',
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
     },
     settings: {
       react: {
@@ -36,4 +38,4 @@ export default [
       },
     },
   },
-]
+])
