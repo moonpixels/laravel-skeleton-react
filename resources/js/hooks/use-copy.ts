80df32d8f78f0610772c3df976ac003dc34e5d95
Adam Hainsworth-Potter <99101334+adamwhp@users.noreply.github.com>
2026-01-04 13:13:10 +0000

    Update dependencies (#68)

--- resources/js/hooks/use-copy.ts
+++ resources/js/hooks/use-copy.ts
@@ -1,34 +1,38 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { useCopyToClipboard } from 'react-use'
 
 export function useCopy() {
-  const [state, copyText] = useCopyToClipboard()
+  const [, copyToClipboard] = useCopyToClipboard()
 
   const [copied, setCopied] = useState(false)
 
   const timeoutRef = useRef<number | null>(null)
 
+  const copyText = useCallback(
+    (text: string) => {
+      copyToClipboard(text)
+      setCopied(true)
+
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+
+      timeoutRef.current = window.setTimeout(() => {
+        setCopied(false)
+        timeoutRef.current = null
+      }, 2000)
+    },
+    [copyToClipboard]
+  )
+
   useEffect(() => {
-    if (!state.value) return
-
-    setCopied(true)
-
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-
-    timeoutRef.current = window.setTimeout(() => {
-      setCopied(false)
-      timeoutRef.current = null
-    }, 2000)
-
     return () => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current)
         timeoutRef.current = null
       }
     }
-  }, [state])
+  }, [])
 
   return {
     copyText,
